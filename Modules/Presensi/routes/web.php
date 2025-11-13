69b3854227c510c404a09c7965734fb9d8702d9d
“RegaRohadi” <regarohadi422@gmail.com>
2025-11-13 21:06:42 +0700

    fix share module

--- Modules/Presensi/routes/web.php
+++ Modules/Presensi/routes/web.php
@@ -3,15 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Presensi\Http\Controllers\PresensiController;
 
-Route::middleware(['auth'])
-    ->group(function () {
-        Route::prefix('/projects')
-            ->name('presensi.')
-            ->group(function () {
-                Route::get('/{project_id}/presensi', [PresensiController::class, 'index'])->name('index');
-                Route::get('/{project_id}/attendances', [PresensiController::class, 'getAttendances'])->name('attendances.get');
-                Route::post('/{project_id}/attendances', [PresensiController::class, 'store'])->name('attendances.store');
-                Route::put('/{project_id}/attendances/{attendanceId}', [PresensiController::class, 'update'])->name('attendances.update');
-                Route::delete('/{project_id}/attendances/{attendanceId}', [PresensiController::class, 'delete'])->name('attendances.delete');
-            });
+Route::middleware(['web', 'auth', 'share.menu:project'])->group(function () {
+    Route::prefix('/projects')->group(function () {
+        Route::get('/{project_id}/presensi', [PresensiController::class, 'index'])->name('presensi.index');
+        Route::get('/{project_id}/attendances', [PresensiController::class, 'getAttendances'])->name('attendances.get');
+        Route::post('/{project_id}/attendances', [PresensiController::class, 'store'])->name('attendances.store');
+        Route::put('/{project_id}/attendances/{attendanceId}', [PresensiController::class, 'update'])->name('attendances.update');
+        Route::delete('/{project_id}/attendances/{attendanceId}', [PresensiController::class, 'delete'])->name('attendances.delete');
+        Route::get('/{project_id}/attendances/{attendanceId}/getAttendancesworkers', [PresensiController::class, 'getAttendancesworkers']);
+        Route::patch('/{projectId}/attendances/{attendanceId}/workers/{userId}', [PresensiController::class, 'updateWorkerStatus'])->name('workers.update');
     });
+});
